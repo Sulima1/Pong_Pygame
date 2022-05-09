a6344b211aa67922da96bb99e0941e02f1f9e136
Matthew Borkowski <55469998+Sulima1@users.noreply.github.com>
2022-05-09 15:25:35 -0400

    Finalized changes to pong pygame

--- pong/ponggame.py
+++ pong/ponggame.py
@@ -9,6 +9,7 @@ from paddle import Paddle
 from ball import Ball
 pygame.init()
 pygame.font.init()
+pygame.mixer.init()
 
 #colours
 WHITE = (192, 192, 192)
@@ -32,16 +33,24 @@ SCORE_FONT = pygame.font.Font("freesansbold.ttf", 40)
 #scores
 scoreA = 0
 scoreB = 0
+WINNING_SCORE = 5
+
+#audio
+BOUNCE_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'BounceEffect.wav'))
 
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Pong')
 
 
-def draw_window(ball, paddleA, paddleB, all_sprites_list, textA, textB):
+def draw_window(ball, paddleA, paddleB, all_sprites_list):
 
     WIN.fill(BACKGROUND)
     pygame.draw.line(WIN, WHITE, [349, 0], [349, 500], 5)
+
+    textA = SCORE_FONT.render(f"{scoreA}", False, WHITE)
+    textB = SCORE_FONT.render(f"{scoreB}", False, WHITE)
+
     all_sprites_list.draw(WIN)
     WIN.blit(textA, (250, 10))
     WIN.blit(textB, (420, 10))
@@ -70,18 +79,21 @@ def handle_ball(ball):
         ball_restart(ball)
     if ball.rect.y > BALL_Y_MAX:
         ball.velocity[1] = -ball.velocity[1]
+        BOUNCE_SOUND.play()
     if ball.rect.y < 0:
         ball.velocity[1] = -ball.velocity[1]
-
+        BOUNCE_SOUND.play()
     return scoreA, scoreB
 
 def ball_restart(ball):
+
     ball.rect.x, ball.rect.y = WIDTH/2, HEIGHT/2
-    ball.velocity[0] *= random.choice((1, -1))
-    ball.velocity[1] *= random.choice((1, -1))
+    ball.velocity[0] = 6 * random.choice((1, -1))
+    ball.velocity[1] = 6 * random.choice((1, -1))
 
 def handle_collision(ball, paddleA, paddleB):
     if pygame.sprite.collide_mask(ball, paddleA) or pygame.sprite.collide_mask(ball, paddleB):
+        BOUNCE_SOUND.play()
         ball.bounce()
 
 
@@ -110,9 +122,20 @@ def main():
                 status = False
                 pygame.quit()
 
-        textA = SCORE_FONT.render(f"{scoreA}", False, WHITE)
-        textB = SCORE_FONT.render(f"{scoreB}", False, WHITE)
+        won = False
+        if scoreA >= WINNING_SCORE:
+            won = True
+            win_text = "Left Player Won! - GG"
+        elif scoreB >= WINNING_SCORE:
+            won = True
+            win_text = "Right Player Won! - GG"
 
+        if won:
+            text = SCORE_FONT.render(win_text, 1, WHITE)
+            WIN.blit(text, (WIDTH/2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2))
+            pygame.display.update()
+            pygame.time.delay(1000)
+            pygame.quit()
 
         all_sprites_list.update()
 
@@ -122,7 +145,7 @@ def main():
         keys_pressed = pygame.key.get_pressed()
         paddle_movement(keys_pressed, paddleA, paddleB)
 
-        draw_window(ball, paddleA, paddleB, all_sprites_list, textA, textB)
+        draw_window(ball, paddleA, paddleB, all_sprites_list)
 
     main()
 
